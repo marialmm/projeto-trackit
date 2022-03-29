fa9e6f7e3231f6d4f23232f7d5872a70384935de
Maria Luiza Mota Marques <maria.lmm.975@gmail.com>
2022-03-29 09:46:12 -0300

    Commit inicial

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./../assets/css/reset.css";
+import "./../assets/css/style.css";
+
+function App(){
+    return(
+        <h1>App</h1>
+    )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+
+const root = document.querySelector('.root');
+const app = <App />;
+
+ReactDOM.render(app, root);
